f98d2cdf2ac794c6e6ae08016f7755293e046179
Jordan Veloso <jrveloso96@gmail.com>
2022-08-05 13:51:57 -0500

    Created delete function on front end.

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
-//This is where code that reaches out to the server to send info back and forth
-//use fetches
+
+const deleteButton = document.querySelector('#delete-button')
+
+deleteButton.addEventListener('click', _ => {
+
+    fetch(`/edit/${deleteButton.dataset.doc}`, {
+        method: 'delete',
+        
+    })
+    .then(response => response.json())
+    //If no no entries to delete
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+})
